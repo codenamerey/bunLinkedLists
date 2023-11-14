d3eaa1b19bd93938307acdac69b1fe218469c8e8
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-11-14 13:59:52 +0800

    Add append method to LinkedList.

--- index.ts
+++ index.ts
@@ -9,4 +9,16 @@ const Node = function(value: string | null, next: Node | null) {
 
 const LinkedList = function() {
     const HEAD = Node(null, null);
+
+    const append = (value: string) => {
+
+        let currentNode: Node = HEAD;
+
+        while(currentNode.next != null) {
+            currentNode = currentNode.next
+        }
+
+        currentNode.next = Node(value, null); 
+    }
+
 }
